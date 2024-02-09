29506585aa5e01467d907460a5a9162d920931cb
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2024-02-09 13:52:28 +0300

    Business Concrete PersonManager oluşturuldu ve içerisine  metotlar yazıldı.

--- Business/Concrete/PersonManager.cs
+++ Business/Concrete/PersonManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Business.Concrete
 {
     public class PersonManager
     {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+        public List<Person> GetList()
+        {
+            return null;
+        }
     }
 }
